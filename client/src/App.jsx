e408e9d9de8d0aa076436335b3c1e07b7405059f
Ashray <soaresashray@yahoo.com>
2024-02-15 11:19:48 -0500

    add register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,17 @@ import "./App.css";
 import IndexPage from "./pages/indexPage";
 import LoginPage from "./pages/LoginPage";
 import Layout from "./Layout";
+import RegisterPage from "./pages/register";
+import axios from "axios";
 
+axios.defaults.baseURL = "http://localhost:4000/";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   );
